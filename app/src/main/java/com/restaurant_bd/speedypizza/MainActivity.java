69e2e36c8df602ebd2b3c2e6c9976d196cc18245
juanmolina2402 <1cajuanmolina@gmail.com>
2021-09-29 14:07:17 -0600

    Implementación submenu

--- app/src/main/java/com/restaurant_bd/speedypizza/MainActivity.java
+++ app/src/main/java/com/restaurant_bd/speedypizza/MainActivity.java
@@ -1,21 +1,49 @@
 package com.restaurant_bd.speedypizza;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         FloatingActionButton btnAcceder  = findViewById(R.id.fab);
         btnAcceder.setOnClickListener(v -> {
             startActivity(new Intent(MainActivity.this, AddOrderActivity.class));
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.itm_about) {
+            startActivity(new Intent(MainActivity.this, AddOrderActivity.class));
+        }else{
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
