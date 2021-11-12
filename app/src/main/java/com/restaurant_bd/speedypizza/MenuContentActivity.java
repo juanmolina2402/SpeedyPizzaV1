8b815c343a7a2339698dabdc53036f4c5aa38774
juanmolina2402 <1cajuanmolina@gmail.com>
2021-11-11 22:57:42 -0600

    Add order design

---
+++ app/src/main/java/com/restaurant_bd/speedypizza/MenuContentActivity.java
@@ -0,0 +1,14 @@
+package com.restaurant_bd.speedypizza;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuContentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_content);
+    }
+}
